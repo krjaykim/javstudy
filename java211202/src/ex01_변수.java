caff8c73d8d170cce1618b425e83a0fbbb3eeb99
김동국 <99251271+krjaykim@users.noreply.github.com>
2022-05-04 17:17:21 +0900

    Add files via upload

---
+++ java211202/src/ex01_변수.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+
+public class ex01_변수 {
+
+	public static void main(String[] args) {
+		
+//변수
+//메소드
+		
+
+     
+	}
+
+}
+			
---
+++ java211202/src/sdfsdfa.java
@@ -0,0 +1,8 @@
+
+public class sdfsdfa {
+
+	abstract class B{
+		abstract void f();
+	}
+	
+}
